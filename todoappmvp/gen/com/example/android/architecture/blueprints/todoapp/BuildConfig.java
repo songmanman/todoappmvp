a021c239e9a6d7bd8ead6ca85e9c25036635aefd
Xenon Chau <cto@ido-game.com>
2017-08-17 17:37:14 +0800

    mvp eclipse

---
+++ todoappmvp/gen/com/example/android/architecture/blueprints/todoapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.android.architecture.blueprints.todoapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
